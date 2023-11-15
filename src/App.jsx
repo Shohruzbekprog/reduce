84442184a64b1c63538b1ae686bc758b996107b7
Shohruzbekprog <shokhruz.akhe@gmail.com>
2023-11-15 11:16:28 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+
+import './App.css'
+import Carts from './Carts';
+import { useGlobalContext } from './context'
+
+function App() {
+
+  const { amount, clearCart, total } = useGlobalContext();
+
+  return (
+    <>
+      <h2>{amount}</h2>
+      
+      <Carts />
+
+      <button onClick={() => clearCart()}>clealAll</button>
+
+      <p>total price { total}</p>
+        
+    </>
+  )
+}
+
+export default App
